c9b8be0bb6d69b317b11ce51fcff4bc5b932fe5f
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-17 16:13:25 -0300

    feat: :lipstick: add NoteView.jsx in src/journal/views

---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,58 @@
+import { Button, Grid, TextField, Typography } from '@mui/material';
+import { SaveOutlined } from '@mui/icons-material';
+import { ImageGallery } from '../components';
+
+export const NoteView = () => {
+	return (
+		<Grid
+			container
+			direction='row'
+			justifyContent='space-between'
+			alignItems='center'
+			sx={{ mb: 1 }}
+		>
+			<Grid item>
+				<Typography
+					fontSize={39}
+					fontWeight='light'
+				>
+					17 de junio, 2023
+				</Typography>
+			</Grid>
+
+			<Grid item>
+				<Button
+					color='primary'
+					sx={{ p: 2 }}
+				>
+					<SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+					Save
+				</Button>
+			</Grid>
+
+			<Grid container>
+				<TextField
+					type='text'
+					variant='filled'
+					fullWidth
+					placeholder='Write your title here'
+					label='Title'
+					sx={{ border: 'none', mb: 2 }}
+				/>
+
+				<TextField
+					type='text'
+					variant='filled'
+					fullWidth
+					multiline
+					placeholder='Write your post here'
+					// label='Post'
+					minRows={10}
+					// sx={{ border: 'none', mb: 1 }}
+				/>
+			</Grid>
+
+			<ImageGallery />
+		</Grid>
+	);
+};
