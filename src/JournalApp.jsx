dd3bc5e4652bf83947e69320f0d6637cac555eec
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-16 21:52:11 -0300

    feat: :tada: first commit
    Inicio de proyecto, estructura

---
+++ src/JournalApp.jsx
@@ -0,0 +1,11 @@
+import { AppRouter } from './router';
+
+const JournalApp = () => {
+	return (
+		<>
+			<AppRouter />
+		</>
+	);
+};
+
+export default JournalApp;
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import { AuthRoutes } from '../auth';
+import { JournalRoutes } from '../journal';
+
+export const AppRouter = () => {
+	return (
+		<Routes>
+			{/* Login y registro */}
+			<Route
+				path='/auth/*'
+				element={<AuthRoutes />}
+			/>
+
+			{/* JournalApp */}
+			<Route
+				path='/*'
+				element={<JournalRoutes />}
+			/>
+		</Routes>
+	);
+};
