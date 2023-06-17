02e807e9847c5d24701096bb967652d6eb5476fa
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-17 17:16:28 -0300

    style: :lipstick: end of layout in src/journal/pages/JournalPage.jsx

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,27 @@
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+import { JornalLayout } from '../layout';
+import { NothingSelectedView, NoteView } from '../views';
+
 export const JournalPage = () => {
-	return <div>JournalPage</div>;
+	return (
+		<JornalLayout>
+			{/* <NothingSelectedView /> */}
+			<NoteView />
+
+			<IconButton
+				size='large'
+				sx={{
+					color: 'white',
+					backgroundColor: 'error.main',
+					':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+					position: 'fixed',
+					right: 50,
+					bottom: 50,
+				}}
+			>
+				<AddOutlined sx={{ fontSize: 30 }} />
+			</IconButton>
+		</JornalLayout>
+	);
 };
