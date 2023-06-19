83eb32ad8f34aae0944852d78d54e21947af52c8
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-19 16:31:32 -0300

    style: :art: organize imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import JournalApp from './JournalApp.jsx';
+import { store } from './store';
 import 'animate.css';
 import './styles.css';
-import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
