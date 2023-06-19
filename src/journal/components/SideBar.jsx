0539d83a4bd4b4e79462ad306efc9fb2ef682319
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-19 16:34:17 -0300

    refactor: :recycle: separate in a component SideBarItem the notes view

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,21 +1,10 @@
 import { useSelector } from 'react-redux';
-import { TurnedInNot } from '@mui/icons-material';
-import {
-	Box,
-	Divider,
-	Drawer,
-	Grid,
-	List,
-	ListItem,
-	ListItemButton,
-	ListItemIcon,
-	ListItemText,
-	Toolbar,
-	Typography,
-} from '@mui/material';
+import { Box, Divider, Drawer, List, Toolbar, Typography } from '@mui/material';
+import { SideBarItem } from './SideBarItem';
 
 export const SideBar = ({ drawerWidth = 240 }) => {
 	const { displayName } = useSelector(state => state.auth);
+	const { notes } = useSelector(state => state.journal);
 
 	return (
 		<Box
@@ -42,25 +31,11 @@ export const SideBar = ({ drawerWidth = 240 }) => {
 				<Divider />
 
 				<List>
-					{['Enero', 'Febrero', 'Marzo', 'Abril'].map((text, i) => (
-						<ListItem
-							key={i}
-							disablePadding
-						>
-							<ListItemButton>
-								<ListItemIcon>
-									<TurnedInNot />
-								</ListItemIcon>
-								<Grid container>
-									<ListItemText primary={text} />
-									<ListItemText
-										secondary={
-											'Ipsum is simply dummy text of the printing and typesetting industry.'
-										}
-									/>
-								</Grid>
-							</ListItemButton>
-						</ListItem>
+					{notes.map(note => (
+						<SideBarItem
+							key={note.id}
+							{...note}
+						/>
 					))}
 				</List>
 			</Drawer>
