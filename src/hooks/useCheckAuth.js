5c7acb73bb8bfad48704f3723c33cf33a238b37d
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-19 16:32:04 -0300

    perf: :zap: include startLoadingNotes

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useDispatch, useSelector } from 'react-redux';
 import { FirebaseAuth } from '../firebase';
-import { login, logout } from '../store/slices/auth';
+import { login, logout, startLoadingNotes } from '../store/slices';
 
 export const useCheckAuth = () => {
 	const { status } = useSelector(state => state.auth);
@@ -13,6 +13,7 @@ export const useCheckAuth = () => {
 			if (!user) return dispatch(logout());
 			const { uid, email, displayName, photoURL } = user;
 			dispatch(login({ uid, email, displayName, photoURL }));
+			dispatch(startLoadingNotes());
 		});
 	}, []);
 
