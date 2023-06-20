4bbbe0fd45f65597444795fc955d27cea6bbe553
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-19 21:36:14 -0300

    feat: :lipstick: update ui, can upload images and delete notes

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,8 +1,70 @@
-import { Button, Grid, TextField, Typography } from '@mui/material';
-import { SaveOutlined } from '@mui/icons-material';
+import { useEffect, useMemo, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, Grid, IconButton, TextField, Typography } from '@mui/material';
+import {
+	DeleteOutline,
+	SaveOutlined,
+	UploadOutlined,
+} from '@mui/icons-material';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
 import { ImageGallery } from '../components';
+import { useForm } from '../../hooks';
+import {
+	setActiveNote,
+	startSaveNote,
+	startUploadinFiles,
+	startDeletingNote,
+} from '../../store';
 
 export const NoteView = () => {
+	const dispatch = useDispatch();
+	const {
+		active: note,
+		messageSaved,
+		isSaving,
+	} = useSelector(state => state.journal);
+
+	const { body, title, date, onInputChange, formState } = useForm(note);
+
+	const dateString = useMemo(() => {
+		const newDate = new Date(date).toUTCString();
+
+		return newDate;
+	}, [date]);
+
+	const fileInputRef = useRef();
+
+	useEffect(() => {
+		dispatch(setActiveNote(formState));
+	}, [formState]);
+
+	useEffect(() => {
+		if (messageSaved.length > 0) {
+			Swal.fire('Updated note', messageSaved, 'success');
+		}
+	}, [messageSaved]);
+
+	const onSaveNote = () => {
+		dispatch(startSaveNote());
+	};
+
+	const onFileinputChange = ({ target }) => {
+		if (target.files === 0) return;
+
+		dispatch(startUploadinFiles(target.files));
+		Swal.fire({
+			title: 'Upload Files',
+			text: 'Images ready to be saved',
+			timer: 2500,
+			icon: 'warning',
+		});
+	};
+
+	const onDelete = () => {
+		dispatch(startDeletingNote());
+	};
+
 	return (
 		<Grid
 			className='animate__animated animate__fadeIn animate__fast'
@@ -14,24 +76,45 @@ export const NoteView = () => {
 		>
 			<Grid item>
 				<Typography
-					fontSize={39}
+					fontSize={26}
 					fontWeight='light'
 				>
-					17 de junio, 2023
+					{dateString}
 				</Typography>
 			</Grid>
 
 			<Grid item>
-				<Button
+				<input
+					type='file'
+					multiple
+					onChange={onFileinputChange}
+					style={{ display: 'none' }}
+					ref={fileInputRef}
+				/>
+				<IconButton
 					color='primary'
-					sx={{ p: 2 }}
+					disabled={isSaving}
+					sx={{ p: 1 }}
+					onClick={() => fileInputRef.current.click()}
+				>
+					<UploadOutlined />
+				</IconButton>
+
+				<Button
+					disabled={isSaving}
+					onClick={onSaveNote}
+					color='primary'
+					sx={{ p: 1 }}
 				>
 					<SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
 					Save
 				</Button>
 			</Grid>
 
-			<Grid container>
+			<Grid
+				container
+				sx={{ mt: 1 }}
+			>
 				<TextField
 					type='text'
 					variant='filled'
@@ -39,6 +122,9 @@ export const NoteView = () => {
 					placeholder='Write your title here'
 					label='Title'
 					sx={{ border: 'none', mb: 2 }}
+					name='title'
+					value={title}
+					onChange={onInputChange}
 				/>
 
 				<TextField
@@ -48,10 +134,28 @@ export const NoteView = () => {
 					multiline
 					placeholder='Write your post here'
 					minRows={10}
+					name='body'
+					value={body}
+					onChange={onInputChange}
 				/>
 			</Grid>
 
-			<ImageGallery />
+			<Grid
+				container
+				justifyContent='end'
+				alignItems='center'
+			>
+				<Button
+					onClick={onDelete}
+					sx={{ mt: 2 }}
+					color='error'
+				>
+					<DeleteOutline />
+					Delete
+				</Button>
+			</Grid>
+
+			<ImageGallery images={note.imageUrls} />
 		</Grid>
 	);
 };
